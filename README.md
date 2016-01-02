6630cd6689f7a8431f60ec57581fdedf7974b2c7
feng1991 <zhangyifengit@163.com>
2016-01-02 15:57:59 +0800

    将项目写成类的形式，修改readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Memcache_admin
-- This is project based on https://github.com/junstor/memadmin.git.
+- This is a project based on https://github.com/junstor/memadmin.git.
 - I add a menu with which we can get all the key-values from memcached without choosing one slab.In the meantime,we can specify the key or the value we want to find.
+
+# dowmload_picture
+- This is a tool to download pictures in the given urls.
+- TO do: 1. use multi-threads. 2. download pictures loaded by js. 3. climb related url. 4. create zip
--- dowmload_picture/index.php
+++ dowmload_picture/index.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * 多线程加速
- * 写成类的形式
- * js加载的图片怎么取
- * 相关链接 爬取
- * 输入网址后打包成压缩文件并下载
- */
-
 	function dump($str,$exit=false){
 		header('content-type:text/html;charset=utf-8');
 		echo '<pre>';
@@ -18,52 +10,94 @@
 		}
 	}
 
+	
+	class DownLoadImg{
+
+		protected $urls;
+
+		/**
+		 * Need urls to download
+		 */
+		public function __construct($urls = false){
+			header('content-type:text/html;charset=utf-8');
+			if($urls){
+				$this->urls = $urls;
+			}
+		}
+
+
+		/**
+		 * get start
+		 */
+		public function start(){
+			foreach($this->urls as $url){
+				$begin_time = microtime(true);
+				$text = $this->get_contents($url);
+				//dump($text,true);
+				$match = $this->get_img_urls($text);
+				//dump($match);
+				$this->save_imgs($url,$match);
+				echo sprintf("use time:%s\n",microtime(true) - $begin_time);	
+			}
+		}
+
+
+		/**
+		 * get the html content in the given url
+		 */
+		protected function get_contents($url){
+			return file_get_contents($url);
+		}
+
+
+		/**
+		 * get the url of the imgae using RegExp
+		 */
+		protected function get_img_urls($text){
+			$pattern = '/<img.+src=\"?(.+\.(jpeg|jpg|gif|bmp|bnp|png|gif))\"?.+>/i';
+			preg_match_all($pattern,$text,$match);
+			return $match;
+		}
+
+
+		/**
+		 * save the imgae by the url
+		 */
+		protected function save_imgs($url,$match){
+			$imgs = $match[1];
+			$type = $match[2];
+			//dump($imgs,true);
+			preg_match('/\w+\:\/\/(.+)\//',$url,$domain);
+			//dump($domain,true);
+			$save_path = sprintf("./imgs/%s/",$domain[1]);
+			if(!is_dir($save_path)){
+				mkdir($save_path,0777,true);
+			}
+			foreach($imgs as $i => $img){
+				!$img && continues;
+				//相对地址转换为绝对地址
+				if($img[0] == '/'){
+					$img = $url.ltrim($img,'/');
+				}
+				$img_content = file_get_contents($img);
+				!$img_content && continues;
+				$new_fileName = $save_path.md5(uniqid('', true)).'.'.$type[$i];
+				file_put_contents($new_fileName, $img_content);
+			}
+		}
+
+	}
+
 
 	
 
 	
 	$urls = array(
-		"http://www.wmpic.me/tupian/yijing/",
-		"http://huaban.com/",
+		"http://www.mm131.com/",
+		"http://www.mmkaixin.com/",
 	);
-	foreach($urls as $url){
-
-		$begin_time = microtime(true);
-
-		//1.获得内容
-		$text = file_get_contents($url);
-		//dump($text,true);
-		
-		//2.获得图片地址
-		$pattern = '/<img.+src=\"?(.+\.(jpeg|jpg|gif|bmp|bnp|png))\"?.+>/i';
-		preg_match_all($pattern,$text,$match);
-		$imgs = $match[1];
-		$type = $match[2];
-		//dump($match);
-		//dump($imgs,true);
-
-		//3.保存图片到本地
-		preg_match('/\w+\:\/\/(.+)\//',$url,$domain);
-		//dump($domain,true);
-		$save_path = sprintf("./imgs/%s/",$domain[1]);
-		if(!is_dir($save_path)){
-			mkdir($save_path,0777,true);
-		}
-		foreach($imgs as $i => $img){
-			!$img && continues;
-			//相对地址转换为绝对地址
-			if($img[0] == '/'){
-				$img = $url.ltrim($img,'/');
-			}
-			$img_content = file_get_contents($img);
-			!$img_content && continues;
-			$new_fileName = $save_path.md5(uniqid('', true)).'.'.$type[$i];
-			file_put_contents($new_fileName, $img_content);
-			//exit;
-		}
-
-		echo sprintf("use time:%s\n",microtime(true) - $begin_time);	
-	}
+	$down = new DownLoadImg($urls);
+	$down->start();
 	
 	
 
